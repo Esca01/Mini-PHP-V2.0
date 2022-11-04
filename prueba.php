1384bf06056ccf9f7362d9dd37e911b0c6a584c5
Esteban Escalante <86449094+Esca01@users.noreply.github.com>
2022-11-04 12:52:35 +0000

    Initial commit

---
+++ prueba.php
@@ -0,0 +1,25 @@
+<?php
+if (($a ==1) || (3!=5)){
+$c = 5;
+}
+
+echo "hello world";
+
+if (($a ==1) || (3!=5)){
+	$c = 5;
+	}elseif($c<$a){
+		echo $c;
+	} else {
+		$a = 6;
+	}
+
+echo "esteban"
+
+function foo($arg1,$a,$arg2){
+	if (($arg1 == 1) || ($a!=$arg2)){
+		$a = 5;
+	}else{
+	    $a = $arg1;
+	}
+}
+?>
